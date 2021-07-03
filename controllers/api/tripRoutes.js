3cf1fbb138e7f82c22aac407b879dc9b9fa97518
Daryl Nauman <darylnauman@gmail.com>
2021-07-03 16:43:31 -0400

    created ability for user to update trip details from front end to back end

--- controllers/api/tripRoutes.js
+++ controllers/api/tripRoutes.js
@@ -20,6 +20,7 @@ router.get('/',async (req, res) => {
   }
 });
 
+// get a specific trip by ID
 router.get('/:id',async (req, res) => {
   try {
     const tripData = await Trip.findByPk(req.params.id, {
@@ -37,6 +38,7 @@ router.get('/:id',async (req, res) => {
   }
 });
 
+// create a new trip
 router.post('/', async (req, res) => {
   try {
     const newTrip = await Trip.create({...req.body, user_id: req.session.user_id});
@@ -47,8 +49,38 @@ router.post('/', async (req, res) => {
   }
 });
 
+// update one trip by its `id` 
+router.put('/:id', async (req, res) => {
+  try {
+    const tripData = await Trip.update({
+      start_date: req.body.start_date,
+      end_date: req.body.end_date,
+      trip_budget: req.body.trip_budget,
+      hotel_cost: req.body.hotel_cost,
+      food_cost: req.body.food_cost,
+      ent_cost: req.body.ent_cost,
+      misc_cost: req.body.misc_cost,
+      transport_cost: req.body.transport_cost
+    },  
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+
+    if(!tripData) {
+      res.status(404).json({ message: 'No trip found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tripData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// delete one trip by its `id` 
 router.delete('/:id',async  (req, res) => {
-  // delete one trip by its `id` 
   try {
     const tripData = await Trip.destroy( {
       where: {
