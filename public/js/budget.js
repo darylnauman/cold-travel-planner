8beac23a44fc99446f6a56e630c3fb9f889d1b03
patrick brodie <patrickxbrodie@hotmail.com>
2021-07-05 10:08:18 -0400

    fixed the conversion

--- public/js/budget.js
+++ public/js/budget.js
@@ -1,14 +1,18 @@
-
+const userID = document.querySelector('#userId').dataset.id;
   // const dropDown = document.querySelector("#dropdown").value
   // const drpSel = document.querySelector(".dropdwn").value
   const exBtn = document.querySelector(".exbtn")
+
+  var pageURL = window.location.href;
+  // var lastURLSegment = pageURL.substr(pageURL.lastIndexOf('trips/') + 1);
+console.log("user id",userID)
 
   const currencyExchange = (event) => {
     event.preventDefault();
 
     let dropDown =document.querySelector('.dropdwn').selectedOptions[0].value
     console.log(`this is the client side: ${dropDown}`)
-    window.location.replace(`/past-trips/1/${dropDown}`);
+    window.location.replace(`/trips/${userID}/${dropDown}`);
 
    }
 
